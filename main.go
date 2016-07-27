0197efe184baa410300d177f0ca7ee6431674441
Ali Waseem <waseema393@gmail.com>
2016-07-26 20:36:39 -0700

    Updated to list todos

--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
-func muliply(x int, y int) int {
-	return x * y
+	"github.com/gorilla/mux"
+)
+
+type todo struct {
+	Done    bool
+	Message string
+}
+
+type todos []todo
+
+var todoList = todos{
+	todo{Done: false, Message: "Get Eggs!"},
+	todo{Done: false, Message: "Get Milk!"},
 }
 
 func main() {
-	fmt.Println(muliply(3, 4))
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/list", ListTodos)
+	log.Println("Running Server on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+// HomeHandler provides route handling for the home route
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("hello world"))
+}
+
+// ListTodos provides route handling for the list todos route
+func ListTodos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content=Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	j, err := json.Marshal(todoList)
+	if err != nil {
+		log.Panic(err)
+	} else {
+		w.Write(j)
+	}
 }
